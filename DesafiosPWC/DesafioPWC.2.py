cf9037f1059a0b406edfe3454dc0a8f69382a4f7
Guilherme Vieira Pereira <84946005+v-gui@users.noreply.github.com>
2024-02-11 17:53:09 -0300

    Update DesafioPWC.2.py

--- DesafiosPWC/DesafioPWC.2.py
+++ DesafiosPWC/DesafioPWC.2.py
@@ -6,12 +6,20 @@ def separar_endereco(endereco):
     for caractere in endereco:
         if caractere.isdigit() and not numero:
             numero = True
-        if not numero and not caractere.isdigit() and not caractere == '' and not caractere == ',':
+        if not numero and not caractere.isdigit() and not caractere == '' and not caractere == ', ':
             nome_rua += caractere
         elif numero:
             numero_rua += caractere
 
-    return [nome_rua + ',' + numero_rua]
+        if "No" in endereco:
+            index_no = endereco.find("No")
+            nome_rua = endereco[:index_no].strip()
+            numero_rua = endereco[index_no:].strip()
+
+    if ',' in endereco:
+        return [nome_rua + numero_rua]
+    else:
+        return [nome_rua + ', ' + numero_rua]
 
 # Testando com os exemplos fornecidos
 
@@ -21,6 +29,11 @@ print(separar_endereco("Cambuí 804B"))
 print(separar_endereco("Rio Branco 23"))
 print(separar_endereco("Quirino dos Santos 23 b"))
 print(separar_endereco("Calle Sagasta, 26"))
+print(separar_endereco("4, Rue de la République"))
+print(separar_endereco("100 Broadway Av"))
+print(separar_endereco("Calle Sagasta, 26"))
+print(separar_endereco("Calle 44 No 1991"))
+
 
 # Já no segundo caso, como há 2 itens com mais de um espaço, o código anterior não funciona mais.
 # Para este caso criei duas strings, uma para o nome e outra para o número.
